152ffd0b6e5e26f2df1fe37b319a4f1149649776
Pawel Szwagierek <szwagier90@gmail.com>
2023-01-03 16:17:10 +0000

    Receive correct html and test template

--- tests.py
+++ tests.py
@@ -13,9 +13,6 @@ class TasksPage(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get('/tasks/')
         html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>Task management site</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTemplateUsed(response, 'tasks/home.html')
--- views.py
+++ views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
 # Create your views here.
 def home_page(request):
-	return HttpResponse("<html><title>Task management site</title></html>")
+	return render(request, 'tasks/home.html')
