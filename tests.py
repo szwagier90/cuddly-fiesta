04bf9aff6ba02774169502fae4057b467fb8b461
Pawel Szwagierek <szwagier90@gmail.com>
2023-01-03 12:46:40 +0000

    Basic view returns minimal html

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 
+from django.http import HttpRequest
 from django.urls import resolve
 
 from .views import home_page
@@ -11,3 +12,10 @@ class TasksPage(TestCase):
         found = resolve('/tasks/')
         self.assertEqual(found.func, home_page)
 
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>Task management site</title>', html)
+        self.assertTrue(html.endswith('</html>'))
--- views.py
+++ views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-	return HttpResponse("")
+	return HttpResponse("<html><title>Task management site</title></html>")
