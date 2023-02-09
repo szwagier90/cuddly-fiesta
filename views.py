6fb837c93930323d119f6091b2c3dcf267ec6d23
Pawel Szwagierek <szwagier90@gmail.com>
2023-02-09 13:44:25 +0000

    Display all tasks in table

--- views.py
+++ views.py
@@ -7,4 +7,5 @@ def home_page(request):
     if request.method == 'POST':
         Task.objects.create(name=request.POST['task_name'])
         return redirect('/tasks/')
-    return render(request, 'tasks/home.html')
+    tasks = Task.objects.all()
+    return render(request, 'tasks/home.html', {'tasks': tasks})
