5b4bef77e47dae9668f37119857faad4f0cb55b0
Pawel Szwagierek <szwagier90@gmail.com>
2023-03-08 13:22:00 +0000

    Refactor - remove redundant code

--- views.py
+++ views.py
@@ -7,8 +7,7 @@ def home_page(request):
     if request.method == 'POST':
         Task.objects.create(name=request.POST['task_name'])
         return redirect('/tasks/first_task_list_url')
-    tasks = Task.objects.all()
-    return render(request, 'tasks/home.html', {'tasks': tasks})
+    return render(request, 'tasks/home.html')
 
 def task_list(request):
     tasks = Task.objects.all()
